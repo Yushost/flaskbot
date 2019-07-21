ecd5d6d439a18b015fa2a0c45bbb1881ceeb0045
Yuseon <mongku90@gmail.com>
2019-07-21 16:29:57 +0900

    homepage

--- yuseon.py
+++ yuseon.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'this is my homepage!'
+
+@app.route('/yuseon')
+def yuseon():
+    return render_template('intro.html')
 
 if __name__ == '__main__':
     app.run()
